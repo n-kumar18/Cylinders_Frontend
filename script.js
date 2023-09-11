cd4ce5531c9b8d4634b4c5d1307e8b22cd0ceb76
Naveen Kumar <naveenkumar709n@gmail.com>
2023-09-11 17:30:49 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,64 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const receiveButton = document.getElementById('receiveButton');
+    const returnButton = document.getElementById('returnButton');
+    const receiveForm = document.getElementById('receiveForm');
+    const returnForm = document.getElementById('returnForm');
+    const receiveSerialNo = document.getElementById('receiveSerialNo');
+    const receiveWeight = document.getElementById('receiveWeight');
+    const returnSerialNo = document.getElementById('returnSerialNo');
+    const returnWeight = document.getElementById('returnWeight');
+    const receiveSubmit = document.getElementById('receiveSubmit');
+    const returnSubmit = document.getElementById('returnSubmit');
+    const receiveMessage = document.getElementById('receiveMessage');
+    const returnMessage = document.getElementById('returnMessage');
+
+    receiveButton.addEventListener('click', () => {
+        receiveForm.style.display = 'block';
+        returnForm.style.display = 'none';
+    });
+
+    returnButton.addEventListener('click', () => {
+        returnForm.style.display = 'block';
+        receiveForm.style.display = 'none';
+    });
+
+    receiveSubmit.addEventListener('click', () => {
+        const serialNo = receiveSerialNo.value;
+        const weight = receiveWeight.value;
+
+        // Send data to the server to handle receiving cylinder
+        fetch('/receive-cylinder', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ serialNo, weight }),
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            receiveMessage.textContent = data.message;
+            receiveSerialNo.value = '';
+            receiveWeight.value = '';
+        });
+    });
+
+    returnSubmit.addEventListener('click', () => {
+        const serialNo = returnSerialNo.value;
+        const weight = returnWeight.value;
+
+        // Send data to the server to handle returning cylinder
+        fetch('/return-cylinder', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ serialNo, weight }),
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            returnMessage.textContent = data.message;
+            returnSerialNo.value = '';
+            returnWeight.value = '';
+        });
+    });
+});
